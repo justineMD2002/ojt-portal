f73efce0b15b9f1f146290af7cb126652d23a534
Unknown <luckyjonesbuayan@gmail.com>
2024-05-27 03:30:55 +0800

    style: training plan

--- client/src/Components/TrainingPlan/TrainingPlan.js
+++ client/src/Components/TrainingPlan/TrainingPlan.js
@@ -3,65 +3,73 @@ import axios from "axios";
 import { useAuth } from "../UserManagement/AuthContext";
 
 const TrainingPlan = () => {
-    const [plans, setPlans] = useState([]);
-    const { authUser } = useAuth();
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get(
-            "https://ojt-portal-backend2.azurewebsites.net/get-student-trainingplans",
-            {
-              params: {
-                studentEmail: authUser.userInfo.email,
-              },
-              headers: {
-                Authorization: `Bearer ${authUser.accessToken}`,
-              },
-            }
-          );
-          console.log(response.data);
-          setPlans(response.data);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
-  
-      fetchData();
-    }, []);
-  
-    return (
-        <div className="TrainingPlan">
-          <h1>Training Plan</h1>
-          {plans.map((item) => (
-            <div key={item.trainingplanid} className="data">
-              <h2>
-                {item.trainingplanid}: {item.description}
-              </h2>
-              <p>Tasks: </p>
-              <ul>
-                {item.tasks.map((task) => (
-                  <li key={task.taskId}>
-                    <div>Task ID: {task.taskId}</div>
-                    <div>Title: {task.title}</div>
-                    <div>Description: {task.description}</div>
-                    <div>Objective: {task.objective}</div>
-                    <p>Skills: </p>
-                    <ul>
-                      {task.skills.map((skill) => (
-                        <li key={skill.skillId}>
-                          {skill.skillName} - {skill.domain}
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-          <button>Upload Logbook</button>
-        </div>
-      );
+  const [plans, setPlans] = useState([]);
+  const { authUser } = useAuth();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://ojt-portal-backend2.azurewebsites.net/get-student-trainingplans",
+          {
+            params: {
+              studentEmail: authUser.userInfo.email,
+            },
+            headers: {
+              Authorization: `Bearer ${authUser.accessToken}`,
+            },
+          }
+        );
+        console.log(response.data);
+        setPlans(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
-  
-  export default TrainingPlan;
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="TrainingPlan">
+      <h1>Training Plan</h1>
+      {plans.map((item) => (
+        <div key={item.trainingplanid} className="data">
+          <h2>
+            {item.trainingplanid}: {item.description}
+          </h2>
+          {item.tasks.length > 0 && <h4>Tasks</h4>}
+          <ul className="task-list">
+            {item.tasks.map((task) => (
+              <li key={task.taskId} className="task">
+                <p className="id">
+                  <span>Task ID:</span> {task.taskId}
+                </p>
+                <p className="title">
+                  <span>Title:</span> {task.title}
+                </p>
+                <p>
+                  <span>Description:</span> {task.description}
+                </p>
+                <p>
+                  <span>Objective:</span> {task.objective}
+                </p>
+                <p className="skills-txt">Skills:</p>
+                <ul>
+                  {task.skills.map((skill) => (
+                    <li key={skill.skillId}>
+                      {skill.skillName} - {skill.domain}
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <button>Upload Logbook</button>
+    </div>
+  );
+};
+
+export default TrainingPlan;
