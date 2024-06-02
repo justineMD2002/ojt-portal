30bd2919ba3159cc9669b90affd8289ddb059c82
Lance Vincent A. Salera <lancevincent.salera@cit.edu>
2024-06-02 18:08:05 +0800

    handled join ojt modal bugs

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,21 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StudentInfo from "./StudentInfo/StudentInfo";
 import { useAuth } from "../UserManagement/AuthContext";
 import JoinOJTTeamModalController from "./JoinOJTModal/controller/JoinOJTModalController";
 import ProgressOverviewController from "./ProgressOverview/controller/ProgressOverviewController";
 import OJTTrackingController from "./OJTTracking/controller/OJTTrackingController";
+import axios from "axios";
 
 const Dashboard = () => {
   const { authUser, handleLogout } = useAuth();
-  const [isOfficialIntern, setIsOfficialIntern] = useState(
-    authUser.ojtRecord.status !== null &&
-      authUser.ojtRecord.status === "ONGOING"
-  );
+  const [isOfficialIntern, setIsOfficialIntern] = useState(true);
+
+  const getOJTRecord = async () => {
+    try {
+      const response = await axios.get(
+        "https://ojt-backend.azurewebsites.net/get-ojt-records",
+
+        {
+          params: {
+            studentEmail: authUser.userInfo.email,
+          },
+          headers: {
+            Authorization: `Bearer ${authUser.accessToken}`,
+          },
+        }
+      );
+
+      console.log(response.data[0].status);
+      if (response.data[0].status === "PENDING") {
+        setIsOfficialIntern(false);
+      } else {
+        setIsOfficialIntern(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getOJTRecord();
+  }, []);
 
   return (
     <div className="Dashboard">
-      {console.log("isOfficialIntern: ", isOfficialIntern)}
-      {console.log(authUser)}
       {!isOfficialIntern && (
         <JoinOJTTeamModalController
           handleLogout={handleLogout}
